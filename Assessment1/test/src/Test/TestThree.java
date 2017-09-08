f44d855c882fc5b8321335018508b1d3389363c4
HannahSwigart6 <hannahdeanswigart@gmail.com>
2017-09-08 08:12:11 -0500

    Add files via upload

---
+++ Assessment1/test/src/Test/TestThree.java
@@ -0,0 +1,16 @@
+package Test;
+
+import javax.swing.JOptionPane;
+public class TestThree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// System.out.println(" I want to get out of this school")
+		
+		JOptionPane.showMessageDialog(null, "I want to get out of this school.");
+		
+		
+	}
+
+}
---
+++ Assessment1/test/src/Test/TestTwo.java
@@ -0,0 +1,10 @@
+package Test;
+import javax.swing.JOptionPane;
+public class TestTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null,"I want to get out of this school");
+	}
+
+}
